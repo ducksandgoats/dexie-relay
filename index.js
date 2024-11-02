ee5550dbb4a27cca4b561944221ea6c85a3a78fb
ducksandgoats <mrtraderjoseph@gmail.com>
2024-11-01 22:19:44 -0500

    make changes

--- index.js
+++ index.js
@@ -182,17 +182,18 @@ export default class Base extends EventEmitter {
                 } else if(datas.session){
                     if(datas.session === 'request'){
                         const stamps = await dataTab.where('stamp').notEqual(0).toArray()
+                        const count = datas.count || 25
                         while(stamps.length){
                             datas.session = 'response'
                             datas.edits = null
-                            datas.stamps = stamps.splice(stamps.length - 25, 25)
+                            datas.stamps = stamps.splice(stamps.length - count, count)
                             this.client.onSend(JSON.stringify(datas), nick)
                         }
                         const edits = await dataTab.where('edit').notEqual(0).toArray()
                         while(edits.length){
                             datas.session = 'response'
                             datas.stamps = null
-                            datas.edits = edits.splice(edits.length - 25, 25)
+                            datas.edits = edits.splice(edits.length - count, count)
                             this.client.onSend(JSON.stringify(datas), nick)
                         }
                     } else if(datas.session === 'response'){
@@ -262,9 +263,10 @@ export default class Base extends EventEmitter {
                         } else {
                             return
                         }
+                        const count = datas.count || 25
                         while(stamps.length){
                             datas.session = 'stamps'
-                            datas.stamps = stamps.splice(stamps.length - 25, 25)
+                            datas.stamps = stamps.splice(stamps.length - count, count)
                             datas.edits = null
                             this.client.onSend(JSON.stringify(datas), nick)
                         }
@@ -304,10 +306,11 @@ export default class Base extends EventEmitter {
                         } else {
                             return
                         }
+                        const count = datas.count || 25
                         while(edits.length){
                             datas.session = 'edits'
                             datas.stamps = null
-                            datas.edits = edits.splice(edits.length - 25, 25)
+                            datas.edits = edits.splice(edits.length - count, count)
                             this.client.onSend(JSON.stringify(datas), nick)
                         }
                     } else if(datas.session === 'edits'){
